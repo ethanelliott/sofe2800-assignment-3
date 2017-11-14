535da29be4a726b5d48672e685c3387e49893e2d
Ethan Elliott <ethan.elliott@hotmail.ca>
2017-11-13 21:27:45 -0500

    lookin' good

--- getImage.php
+++ getImage.php
@@ -1,5 +1,5 @@
 <?php
-$q = intval($_GET['q']);
+$hotelid = intval($_GET['hotelid']);
 $servername = "localhost";
 $username =   "ethanell_hbs";
 $password =   "password123";
@@ -8,15 +8,13 @@ $con = new mysqli($servername, $username, $password, $dbname);
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
-mysqli_select_db($con,"city");
-$sql="SELECT * FROM city WHERE Cty_province_id = '".$q."'";
+mysqli_select_db($con,"pictures");
+$sql="SELECT * FROM pictures WHERE hotel_id = '" . $hotelid . "'";
 $result = mysqli_query($con,$sql);
 if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		echo "<option value=". $row["Cty_id"].">" . $row['Cty_Name'] . "</option>";
-	}
-} else {
-	echo "<option value='err'>No Results</option>";
+  while($row = $result->fetch_assoc()) {
+      echo base64_encode($row['image']);
+  }
 }
 
 mysqli_close($con);
